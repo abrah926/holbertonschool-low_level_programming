f9e9b1dd2879f84d8f6483000c9251ebb02f1af1
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-17 13:52:42 -0400

    printf func

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,14 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: Prints a message using puts function.
- *              Escapes quotes within the string.
+ * Description: Prints a specific string with proper grammar.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("\"with proper grammar, but the outcome is a piece of art,");
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
     return (0);
 }
+
 
