7a3a9177591ddcec658db90b9fb2bef079b5fb3d
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-17 13:48:51 -0400

    new printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message using puts function.
+ *              Escapes quotes within the string.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("\"with proper grammar, but the outcome is a piece of art,");
+    return (0);
+}
+
