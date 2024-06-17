570a849eb87360c6441b5fb8da531cbc2db597d4
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-17 13:39:38 -0400

    proper betty comments

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message using puts function.
+ *              Escapes quotes within the string.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
 
