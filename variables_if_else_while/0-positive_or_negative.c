48ede01f8e7c44f2f68fbc64f70366eeb0b4edd4
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-18 09:44:06 -0400

    returns all integers

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+
+	printf("%d", n)
+	return (0);
+}
