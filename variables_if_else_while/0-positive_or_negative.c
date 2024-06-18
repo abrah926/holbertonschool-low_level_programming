6bdcd50e6bf9c58a2f2222cdeb3f134228955e30
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-18 10:47:44 -0400

    positive and neg nums

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,8 +8,8 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
 
     if (n > 0)
     {
@@ -25,6 +25,8 @@ int main(void)
     }
 
 
-	
-	return (0);
-}
+
+        return (0);i}
+~
+~
+~
