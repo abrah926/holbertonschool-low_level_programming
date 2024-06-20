5b2e5966d42a357fd975357a30d654e0e66951ef
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-20 11:39:50 -0400

    prints most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 excluding 2 and 4.
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
