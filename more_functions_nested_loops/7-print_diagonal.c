998f1ad136e8e98b0edf5d1c896c29cbd1f1d3e4
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-20 12:35:15 -0400

    draws a diagonal line in terminal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,7 +4,6 @@
  * print_diagonal - Draws a diagonal line using the \ character.
  * @n: The number of \ characters to be printed.
  */
-
 void print_diagonal(int n)
 {
 	int len, space;
@@ -14,7 +13,7 @@ void print_diagonal(int n)
 		for (len = 0; len < n; len++)
 		{
 			for (space = 0; space < len; space++)
-				_putchar('');
+				_putchar(' ');
 			_putchar('\\');
 
 			if (len == n - 1)
@@ -24,5 +23,5 @@ void print_diagonal(int n)
 		}
 	}
 
-		_putchar('\n');
+	_putchar('\n');
 }
