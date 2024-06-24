314890998912107eeb642d768b5d7d9283efee21
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-24 09:37:33 -0400

    updates value of int to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/* reset_to_98.c */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of the integer pointed to by n to 98.
+ * @n: Pointer to an int to be updated.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
