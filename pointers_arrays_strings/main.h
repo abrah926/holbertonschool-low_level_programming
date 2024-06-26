ceb2b08cefd3abc4f1edd23b85375ec6ae90b95c
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-06-26 11:32:45 -0400

    concatenates strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,26 @@ char *string_toupper(char *);
 char *cap_string(char *);
 int _strlen(char *s);
 void print_rev(char *s);
-#endif
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endiff
