70ab102382d6e716435f98907b3b4d87e83319fd
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-04 23:49:02 -0400

    allocates mem for an array, using malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,42 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 
+char *_memset(char *s, char b, unsigned int n);
 /**
- * string_nconcat - concatenates two strings
+ * _calloc - allocates memory for an array of nmemb elements of size bytes
  *
- * @s1: string source
- * @s2: string destination
- * @n: number of bytes
+ * @nmemb: number of elements
+ * @size: size of elements
  *
- * Return: NULL if function fails
- * pointer to newly allocated space in memory
+ * Return: pointer to the allocated memory
  *
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int x, y, l1, l2;
-	char *p;
+	void *ptr;
 
-	l1 = 0;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (x = 0; s1[x]; ++x)
-		++l1;
-	l2 = l1 + n;
-	p = malloc(sizeof(char) * (l2 + 1));
-	if (!p)
+	if (!nmemb || !size)
 		return (NULL);
-	for (x = 0; x < l1; ++x)
-	{
-		p[x] = s1[x];
-	}
-	for (y = 0; y < n; ++y, ++x)
-	{
-		p[x] = s2[y];
-	}
-	p[x] = '\0';
-	return (p);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
+/**
+ * _memset - fills memory
+ *
+ * @s: memory area
+ * @n: number of bytes
+ * @b: constant byte
+ *
+ * Return: pointer to memory area
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		*(s + x) = b;
+	return (s);
 }
