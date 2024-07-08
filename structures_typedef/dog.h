097c719d3b834764633c8dee02f8d5f4540a8976
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-08 10:57:58 -0400

    declaring struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - for dog attributes 
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner dog
+ *
+ */
+
+struct dog 
+{
+  char *name;
+  float *age;
+  char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ *
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
+
+
