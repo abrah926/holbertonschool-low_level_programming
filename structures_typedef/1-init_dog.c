39b4d71b6e230535552ae842cc2df9df343df808
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-08 11:46:27 -0400

    initializes var in struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialises a variable of type struct dog
+ *
+ * @d: pointer to struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dg
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}:
