85afe6c09dd41ca7471faf81db5d94cce75a90e0
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-27 07:57:35 -0400

    updated files to stop gcc flags

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,13 +14,12 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 	if (h->str == NULL)
-		printf("[0] (nil\n)");
+		printf("[0] (nil)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
 
 	count++;
 	h = h->next;
-	}
-	
-	return count;
+	}	
+	return (count);
 }
