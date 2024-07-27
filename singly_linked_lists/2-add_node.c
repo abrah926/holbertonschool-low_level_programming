bca29c3ee0edbdcd0049af3ddb94dc0d39cebd52
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-27 08:46:00 -0400

    func that adds a new node at the begining of the list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to be duplicated and added to the list
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *dup_str;
+	unsigned int str_len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return (NULL);
+
+	while (str[str_len])
+		str_len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(dup_str);
+		return (NULL);
+	}
+
+	new_node->str = dup_str;
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
