c60819eb96128f765670aa7f5f18bb1fce03c7af
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-27 08:02:04 -0400

    updated files to stop gcc flags

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,16 +16,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
-/*
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
-*/
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
