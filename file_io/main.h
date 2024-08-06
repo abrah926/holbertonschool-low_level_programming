a18f6c938945a5d5ce449225bf4448251961ee83
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-06 15:36:16 -0400

    updated header file

--- file_io/main.h
+++ file_io/main.h
@@ -6,10 +6,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int code, const char *msg, const char *arg);
 
-#endif
+#endif 
+
