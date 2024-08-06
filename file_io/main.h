6cfc11d268378606f98baac713771cfec1026520
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-06 18:46:48 -0400

    updated header file

--- file_io/main.h
+++ file_io/main.h
@@ -7,12 +7,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(int code, const char *msg, const char *arg);
+void error_exit(int code, const char *msg, ...);  /* Updated prototype */
 
-#endif 
+#endif
 
