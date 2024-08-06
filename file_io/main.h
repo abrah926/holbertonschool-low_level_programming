4d247843a2a40067aecf74a99e409eb2a7cc0f0e
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-06 19:13:24 -0400

    updated header file

--- file_io/main.h
+++ file_io/main.h
@@ -1,19 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTONH
+#define HOLBERTONH
 
 #include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdarg.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(int code, const char *msg, ...);  /* Updated prototype */
+
+int _putchar(char c);
 
 #endif
-
